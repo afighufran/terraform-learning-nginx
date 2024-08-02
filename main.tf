dfe0bab9a84f0a53291d64c0b7e16adf7ec13b99
afighufran <leticiahd132@gmail.com>
2024-08-03 02:49:32 +0700

    edit main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,7 @@ resource "null_resource" "web_server" {
       "sudo apt-get update",
       "sudo apt-get install -y nginx",
       "echo 'salam dari binjai, belajar terraform diks' | sudo tee /var/www/html/index.html",
-      "sudo apt-get install -y mysql-server",
-      #   "sudo mysql_secure_installation",
-      #   "sudo mysql -e \"CREATE DATABASE testdatabase;\"",
-      #   "sudo mysql -e \"CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'testpassword';\"",
-      #   "sudo mysql -e \"GRANT ALL PRIVILEGES ON testdatabase.* TO 'testuser'@'localhost';\"",
-      #   "sudo mysql -e \"FLUSH PRIVILEGES;\"",
+      "sudo apt-get install -y mysql-server"
     ]
 
     connection {
